6a296531ab4ab7ac9d40ebeb1863fee92aa8a646
Xdart <xdart.mm2@gmail.com>
2026-01-30 15:25:12 +0300

    Update NoMoreDarkness.java

--- src/main/java/dev/xdart/nomoredarkness/NoMoreDarkness.java
+++ src/main/java/dev/xdart/nomoredarkness/NoMoreDarkness.java
@@ -6,5 +6,7 @@ public class NoMoreDarkness implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        // This is an empty file containing nothing. 
+        // The code in this file is always executed, regardless of the client type: client-side or server-side.
     }
 }
